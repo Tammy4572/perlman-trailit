eff3a58ec9d0f536a5009b79eca62459b1692c70
Paul Haygood <paulthaygood@Pauls-MacBook-Air.local>
2017-07-31 17:01:29 -0400

    init

---
+++ src/components/Featuredloc.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Featuredloc extends Component {
+  render() {
+    return (
+      <div>
+        I am the Featuredloc section
+      </div>
+    );
+  }
+}
+
+export default Featuredloc;
---
+++ src/components/Searchbox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Searchbox extends Component {
+  render() {
+    return (
+      <div>
+        I am the Searchbox
+      </div>
+    );
+  }
+}
+
+export default Searchbox;
